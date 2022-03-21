5ffbba30a08daf805485bfcb695d727d9e58005f
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2022-03-21 15:08:43 -0400

    added enum

---
+++ src/main/java/com/tomoaki/medicalcenterapi/model/enums/Difficulty.java
@@ -0,0 +1,7 @@
+package com.tomoaki.medicalcenterapi.model.enums;
+
+public enum Difficulty {
+	EASY,
+	MEDIUM,
+	HARD
+}
---
+++ src/main/java/com/tomoaki/medicalcenterapi/model/enums/ProblemType.java
@@ -0,0 +1,8 @@
+package com.tomoaki.medicalcenterapi.model.enums;
+
+public enum ProblemType {
+	ALGORITHMS,
+	DATABASE,
+	SHELL,
+	CONCURRENCY
+}
