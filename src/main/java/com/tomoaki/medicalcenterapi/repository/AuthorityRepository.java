0204952f4529cafca556f212c2147354761df374
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2022-04-17 02:22:13 -0400

    Implemented method that get flux of roles by userID

--- src/main/java/com/tomoaki/medicalcenterapi/repository/AuthorityRepository.java
+++ src/main/java/com/tomoaki/medicalcenterapi/repository/AuthorityRepository.java
@@ -1,10 +1,14 @@
 package com.tomoaki.medicalcenterapi.repository;
 
 import com.tomoaki.medicalcenterapi.model.entity.UserRolePair;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface AuthorityRepository extends ReactiveCrudRepository<UserRolePair, Long> {
-
+	
+	@Query("SELECT Role from RoleAssignment where User_ID = :userID")
+	Flux<String> getRolesByUserID(Long userID);
 }
